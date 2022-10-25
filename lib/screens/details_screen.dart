a41370cc57a802a0446b7f57d5cb27053ffd04ae
Erick Estrada <eof105@gmail.com>
2022-10-25 14:55:19 -0500

    MovieDetails - Silvers  - SilverAppBar

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -5,9 +5,40 @@ class DetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Details Screen'),
+    final String movie = ModalRoute.of(context)?.settings.arguments.toString() ?? 'no-movie';
+
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          _CustomAppbar(),
+        ],
+      ),
+    );
+  }
+}
+
+class _CustomAppbar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Colors.indigo,
+      expandedHeight: 200,
+      floating: false,
+      pinned: true,
+      flexibleSpace: FlexibleSpaceBar(
+        centerTitle: true,
+        titlePadding: const EdgeInsets.all(0),
+        title: Container(
+          width: double.infinity,
+          alignment: Alignment.bottomCenter,
+          color: Colors.black12,
+          child: const Text('movie.title', style: TextStyle(fontSize: 16),),
+        ),
+        background: const FadeInImage(
+          placeholder: AssetImage('assets/loading.gif'),
+          image: NetworkImage('https://via.placeholder.com/500x300'),
+          fit: BoxFit.cover,
+        ),
       ),
     );
   }
