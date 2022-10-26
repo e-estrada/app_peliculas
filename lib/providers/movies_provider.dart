e7e9b433522dc05d862c54261a9624079de7e20d
Erick Estrada <eof105@gmail.com>
2022-10-25 21:37:14 -0500

    Peticiones Http y Mapeo de peticiones Http

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -1,13 +1,27 @@
-
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:app_peliculas/models/models.dart';
 
-class MoviesProviders extends ChangeNotifier{
+class MoviesProviders extends ChangeNotifier {
+  final String _baseUrl = 'api.themoviedb.org';
+  final String _apiKey = '3e1635d58e1324644d4f6ba4bbdb0c14';
+  final String _language = 'es-ES';
+
   MoviesProviders() {
     print('MoviesProviders inicializado');
     this.getOnDisplayMovies();
   }
 
   getOnDisplayMovies() async {
-    print('getOnDisplayMovies');
+    var url = Uri.https(_baseUrl, '/3/movie/now_playing', {
+      'api_key': _apiKey,
+      'language': _language,
+      'page': '1',
+    });
+    // Await the http get response, then decode the json-formatted response.
+    var response = await http.get(url);
+    final nowPlayingResponse = NowPlayingResponse.fromJson(response.body);
+    
+    // print(nowPlayingResponse.results[1].title);
   }
 }
