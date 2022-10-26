b463adfeef0b138726163c6491be9ed9a2c55509
Erick Estrada <eof105@gmail.com>
2022-10-26 18:48:29 -0500

    Mostrar actores y mantener en memoria

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -43,7 +43,9 @@ class MoviesProvider extends ChangeNotifier {
   }
 
   Future<List<Cast>> getMovieCast(int movieId) async {
-    print('getMovieCast');
+    
+    if (moviesCast.containsKey(movieId)) return moviesCast[movieId]!;
+
     final jsonData = await _getJsonData('/3/movie/$movieId/credits');
     final creditsResponse = CreditsResponse.fromJson(jsonData);
 
